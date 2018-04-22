ee0c6eb845e41a409d77059cbf164441efac2aa3
Carlos Pantelides <carlos.pantelides@gmail.com>
2018-04-22 17:57:34 -0300

    preparing for edu-ciaa-nxp

---
+++ msp430fr6989/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr6989.cmd 
+
+C_SRCS += \
+../main.c \
+../myClocksWithCrystals.c \
+../myLcd.c 
+
+C_DEPS += \
+./main.d \
+./myClocksWithCrystals.d \
+./myLcd.d 
+
+OBJS += \
+./main.obj \
+./myClocksWithCrystals.obj \
+./myLcd.obj 
+
+OBJS__QUOTED += \
+"main.obj" \
+"myClocksWithCrystals.obj" \
+"myLcd.obj" 
+
+C_DEPS__QUOTED += \
+"main.d" \
+"myClocksWithCrystals.d" \
+"myLcd.d" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../myClocksWithCrystals.c" \
+"../myLcd.c" 
+
+
